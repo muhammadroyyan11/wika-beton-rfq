544320562a5a1a42d7e5ccebbdf54397d63c137d
Muhammad Royyan Zamzami <royyanmz87@gmail.com>
2023-10-30 11:09:56 +0700

    tes

--- application/modules/core/views/menu.php
+++ application/modules/core/views/menu.php
@@ -2,9 +2,10 @@
 <div class="main-menu menu-fixed menu-dark menu-accordion menu-shadow" data-scroll-to-active="true">
   <div class="navbar-header">
     <ul class="nav navbar-nav flex-row">
-      <li class="nav-item mr-auto"><a class="navbar-brand" href="<?= base_url() ?>assets/html/ltr/vertical-menu-template-semi-dark/index.html">
+      <li class="nav-item mr-auto">
+        <a class="navbar-brand" href="<?= base_url() ?>cpanel/dashboard">
           <div class="feather icon-copyrigth"></div>
-          <h2 class="brand-text mb-0"><i class="feather icon-coffee"></i>Tesbeh</h2>
+          <h2 class="brand-text mb-0"><i class="feather icon-coffee"></i>Wika Beton</h2>
         </a></li>
       <li class="nav-item nav-toggle"><a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse"><i class="feather icon-x d-block d-xl-none font-medium-4 primary toggle-icon"></i><i class="toggle-icon feather icon-disc font-medium-4 d-none d-xl-block collapse-toggle-icon primary" data-ticon="icon-disc"></i></a></li>
     </ul>
