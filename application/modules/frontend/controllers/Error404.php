020b4d76fea6de64a5328f0d1a95547c52848924
Besta Alfidi <alfidibesta@gmail.com>
2023-11-05 19:03:55 +0700

    add error page

--- application/modules/frontend/controllers/Error404.php
+++ application/modules/frontend/controllers/Error404.php
@@ -13,17 +13,6 @@ class Error404 extends CI_Controller
 
     public function index()
     {
-
-        // $data = [
-        //     'title' => 'Tentang Perusahaan',
-        //     'company' => $this->db->get('company')->result_array(),
-        //     'pimpinan' => $this->db->get('pimpinan')->result_array(),
-        //     'struktur_organisasi' => $this->db->get('struktur_organisasi')->result_array(),
-        //     'footer_alamat_utama' => $this->db->get('footer_alamat_utama')->result_array(),
-        //     'footer_alamat_cabang' => $this->db->get('footer_alamat_cabang')->result_array(),
-        //     'social_network' => $this->db->get('social_network')->result_array(),
-        // ];
-        // var_dump($data);
         $this->load->view('notfound');
     }
 }
