33ffb3c35b56886c72a7765cb304ee9047a42a70
Besta Alfidi <alfidibesta@gmail.com>
2023-11-13 10:56:05 +0700

    add format number phone

--- application/modules/frontend/controllers/Portfolio.php
+++ application/modules/frontend/controllers/Portfolio.php
@@ -13,13 +13,18 @@ class Portfolio extends CI_Controller
 
     public function index()
     {
+
+        $phone = $this->db->select('no_hp')->get('footer_alamat_utama')->result_array();
+        $editNo = $this->tambahkanTanda($phone[0]['no_hp']);
+
         $data = [
-          'title'       => 'Portfolio',
-          'portfolio'    => $this->db->get('portfolio')->result_array(),
-          'company' => $this->db->get('company')->result_array(),
-          'footer_alamat_utama' => $this->db->get('footer_alamat_utama')->result_array(),
-          'footer_alamat_cabang' => $this->db->get('footer_alamat_cabang')->result_array(),
-          'social_network' => $this->db->get('social_network')->result_array(),
+            'title' => 'Portfolio',
+            'portfolio' => $this->db->get('portfolio')->result_array(),
+            'company' => $this->db->get('company')->result_array(),
+            'footer_alamat_utama' => $this->db->get('footer_alamat_utama')->result_array(),
+            'footer_alamat_cabang' => $this->db->get('footer_alamat_cabang')->result_array(),
+            'social_network' => $this->db->get('social_network')->result_array(),
+            'phone' => $editNo,
         ];
 
         // $dd = [
@@ -27,5 +32,31 @@ class Portfolio extends CI_Controller
         // ];
         // var_dump($dd);
         $this->frontend->load('frontend/template', 'frontend/portfolio/portfolio', $data);
+    }
+    private function tambahkanTanda($teks)
+    {
+        $panjang = strlen($teks);
+        $hasil = '';
+
+        for ($i = 0; $i < $panjang; $i++) {
+            // Tambahkan tanda - setiap 2 karakter setelah karakter pertama
+            if ($i == 2) {
+                $hasil .= '-';
+            }
+
+            // Tambahkan tanda - setiap 4 karakter setelah karakter kedua
+            if ($i == 6) {
+                $hasil .= '-';
+            }
+
+            // Tambahkan tanda - setiap 4 karakter setelah karakter keenam
+            if ($i == 10) {
+                $hasil .= '-';
+            }
+
+            // Tambahkan karakter ke hasil
+            $hasil .= $teks[$i];
+        }
+        return $hasil;
     }
 }
--- application/modules/frontend/controllers/Product.php
+++ application/modules/frontend/controllers/Product.php
@@ -13,6 +13,10 @@ class Product extends CI_Controller
 
     public function index()
     {
+        
+        $phone = $this->db->select('no_hp')->get('footer_alamat_utama')->result_array();
+        $editNo = $this->tambahkanTanda($phone[0]['no_hp']);
+
         $data = [
           'title'       => 'Product Dan Jasa',
           'product'    => $this->db->get('product')->result_array(),
@@ -21,8 +25,36 @@ class Product extends CI_Controller
           'footer_alamat_utama' => $this->db->get('footer_alamat_utama')->result_array(),
           'footer_alamat_cabang' => $this->db->get('footer_alamat_cabang')->result_array(),
           'social_network' => $this->db->get('social_network')->result_array(),
+          'phone' => $editNo,
         ];
         // var_dump($data);
         $this->frontend->load('frontend/template', 'frontend/product/product', $data);
+    }
+
+    private function tambahkanTanda($teks)
+    {
+        $panjang = strlen($teks);
+        $hasil = '';
+
+        for ($i = 0; $i < $panjang; $i++) {
+            // Tambahkan tanda - setiap 2 karakter setelah karakter pertama
+            if ($i == 2) {
+                $hasil .= '-';
+            }
+
+            // Tambahkan tanda - setiap 4 karakter setelah karakter kedua
+            if ($i == 6) {
+                $hasil .= '-';
+            }
+
+            // Tambahkan tanda - setiap 4 karakter setelah karakter keenam
+            if ($i == 10) {
+                $hasil .= '-';
+            }
+
+            // Tambahkan karakter ke hasil
+            $hasil .= $teks[$i];
+        }
+        return $hasil;
     }
 }
