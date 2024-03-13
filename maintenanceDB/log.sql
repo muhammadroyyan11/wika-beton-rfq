5c8c0955f7d06d6658dda4ab3c9217f70b5a8faa
besta <alfidibesta@gmail.com>
2024-03-13 10:08:47 +0700

    update log

--- maintenanceDB/log.sql
+++ maintenanceDB/log.sql
@@ -3,7 +3,7 @@ ALTER TABLE `rfq_request` CHANGE `omzet_kontrak` `omzet_kontrak` VARCHAR(100) NO
 -- project 3
 ALTER TABLE `rfq_request` 
 ADD `pic_se` TEXT NOT NULL AFTER `RFQNumber`, 
-ADD `total_vol` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0' AFTER `pic_se`, 
-ADD `lkb` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0' AFTER `total_vol`, 
+ADD `total_vol` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0' AFTER `pic_se`,
+ADD `lkb` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0' AFTER `total_vol`, 
 ADD `tgl_penawaran` DATE NULL DEFAULT NULL AFTER `lkb`, 
 ADD `p_ke` TEXT NOT NULL AFTER `tgl_penawaran`;
